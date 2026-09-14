c2025ad0eea44dfca1702cf3c3e6ed09ba93939a
000680053 <tamika.mullan@student.tafesa.edu.au>
2026-09-14 10:36:54 +0930

    Ticket prices adjusted - 2nd round

--- Ticketing/TicketPrice.cs
+++ Ticketing/TicketPrice.cs
@@ -14,8 +14,8 @@ namespace Ticketing
         private decimal amountDue;
         private decimal mPrice;
 
-        const decimal mdecBalcony = 37.5m;
-        const decimal mdecGeneral = 29.5m;
+        const decimal mdecBalcony = 38.0m;
+        const decimal mdecGeneral = 29.75m;
         const decimal mdecBox = 65.0m;
         const decimal mdecDiscount = 5.5m; 
 
